80d0419271b50d8b41ea89a95e6caac425cfd9e0
kieraska <kieraska@gmail.com>
2019-12-20 02:51:41 +0100

    add putPersonWithNotExistingId test to InformationControllerPutTest

--- src/test/java/org/vistula/restassured/information/InformationControllerPutTest.java
+++ src/test/java/org/vistula/restassured/information/InformationControllerPutTest.java
@@ -17,7 +17,7 @@ import static org.hamcrest.Matchers.greaterThan;
 
 public class InformationControllerPutTest extends RestAssuredTest {
     @Test
-    public void putPerson() {
+    public void putPersonWithExistingId() {
         JSONObject requestParams = new JSONObject();
         String myName = RandomStringUtils.randomAlphabetic(10);
         String myNationality = RandomStringUtils.randomAlphabetic(10);
@@ -27,23 +27,18 @@ public class InformationControllerPutTest extends RestAssuredTest {
         int mySalary = randomSalary.nextInt((maxSalary - minSalary) + 1) + minSalary;
         int myId = 2;
 
-//        Random randomId = new Random();
-//        long maxId = 10000;
-//        long minId = 1;
-//        long myId = randomId.nextLong((maxId - minId) + 1) + minId;
-
         requestParams.put("name", myName);
         requestParams.put("nationality", myNationality);
         requestParams.put("salary", mySalary);
         requestParams.put("id", myId);
 
-       Information information = given().header("Content-Type", "application/json")
+        Information information = given().header("Content-Type", "application/json")
                 .body(requestParams.toString())
                 .put("/information/" + myId)
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("nationality", equalTo(myNationality) )
+                .body("nationality", equalTo(myNationality))
                 .body("name", equalTo(myName))
                 .body("salary", equalTo(mySalary))
                 .body("id", equalTo(myId))
@@ -53,5 +48,31 @@ public class InformationControllerPutTest extends RestAssuredTest {
         assertThat(information.getSalary()).isEqualTo(mySalary);
         assertThat(information.getNationality()).isEqualTo(myNationality);
         assertThat(information.getId()).isEqualTo(myId);
+    }
+
+
+    @Test
+    public void putPersonWithNotExistingId() {
+        JSONObject requestParams = new JSONObject();
+        String myName = RandomStringUtils.randomAlphabetic(10);
+        String myNationality = RandomStringUtils.randomAlphabetic(10);
+        Random randomSalary = new Random();
+        int maxSalary = 5000;
+        int minSalary = 1000;
+        int mySalary = randomSalary.nextInt((maxSalary - minSalary) + 1) + minSalary;
+        int myId = 100;
+
+        requestParams.put("name", myName);
+        requestParams.put("nationality", myNationality);
+        requestParams.put("salary", mySalary);
+        requestParams.put("id", myId);
+
+        given().header("Content-Type", "application/json")
+                .body(requestParams.toString())
+                .put("/information/" + myId)
+                .then()
+                .log().all()
+                .statusCode(406)
+                .body(equalTo("ID not found. Please use POST method to create new entries"));
     }
 }
